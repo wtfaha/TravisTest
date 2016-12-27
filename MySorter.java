3437eca45616c68f0cac5070f580bd2cd5d6a8cf
wtfaha <h4826111@gmail.com>
2016-12-28 00:25:52 +0800

    提交訊息

---
+++ MySorter.java
@@ -0,0 +1,33 @@
+public class MySorter
+{
+	public void sort (double[] d)
+	{
+		boolean isNagative = true;	//false has nagative//true none nagative
+		for (int i = 0; i < d.length; i++)
+		{
+			if(d[i] <= 0)
+				isNagative = false;	
+		}
+		
+		if(isNagative)	//isNagative == true
+		{
+			for (int i = 0; i < d.length - 1; i++)
+			{
+				for (int j = 0; j < d.length - i - 1; j++)
+				{
+					if (d[j] > d[j + 1])
+					{
+						swap(d, j, j + 1);
+					}
+				}
+			}
+		}
+	}
+
+	public void swap (double d[], int k, int s)
+	{
+		double temp = d[k];
+		d[k] = d[s];
+		d[s] = temp;
+	}
+}
